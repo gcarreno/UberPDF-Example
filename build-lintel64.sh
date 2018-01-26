265560677a093e7623df5f6871f7e7b0ef147c3b
Gustavo Carreno <guscarreno@gmail.com>
2018-01-26 21:07:11 +0000

    Both dynamic amd static scripts now only build if dependencies are present.

--- build-lintel64.sh
+++ build-lintel64.sh
@@ -11,70 +11,80 @@ LAZBUILD_BIN=$LAZARUS_DIR/lazbuild
 ## This script assumes that the UberBuild is in the following folders
 ## under the current path
 UBER_SDK=./uber
-UBER_PDF_LIB_DIR=$UBER_SDK/uberbaselibs/uberpdfsdk/lib/lintel64/gcc/librtl
+UBER_PLATFORM=lintel64
+UBER_PDF_LIB_DIR=$UBER_SDK/uberbaselibs/uberpdfsdk/lib/$UBER_PLATFORM/gcc/librtl
 UBER_PDF_LIB=$UBER_PDF_LIB_DIR/libuberpdfsdkdyn.so
 ##
 
 ## Build variables
 EXAMPLE_BIN=UberPDFExample
-EXAMPLE_NEW_BIN=UberPDFExample-lintel64
-EXAMPLE_BUILD=lintel64
-EXAMPLE_FILE=UberPDFExample.lpi
+EXAMPLE_NEW_BIN=$EXAMPLE_BIN-$UBER_PLATFORM
+EXAMPLE_BUILD=$UBER_PLATFORM
+EXAMPLE_FILE=$EXAMPLE_BIN.lpi
 ##
 
+## Dependencies
 DEPS_OK=0
 
-function clean() {
-  echo "BUILD: Clearing the binaries ( $EXAMPLE_BIN; $EXAMPLE_NEW_BIN )"
-  echo
-  rm -f ./$EXAMPLE_BIN ./$EXAMPLE_NEW_BIN
+function check_dependencies() {
+    echo BUILD: Dependencies ...
+    echo BUILD: Checking lazbuild binary
+    if [ -x $LAZBUILD_BIN ]; then
+        DEPS_OK=1
+    else
+        echo BUILD ERROR: Cannot find lazbuild binary
+        echo SEARCH: $LAZBUILD_BIN
+        echo
+        exit 1
+    fi
+    echo BUILD: Checking for the UberPDF .so library
+    if [ -f $UBER_PDF_LIB ]; then
+        DEPS_OK=1
+    else
+        echo BUILD ERROR: Cannot find UberPDF .so library
+        echo SEARCH: $UBER_PDF_LIB
+        echo Without this library the binary will not run with success
+        echo
+        exit 1
+    fi
+    echo BUILD: Done
+    echo
 }
 
-function check_dependencies() {
-  if [ -x $LAZBUILD_BIN ]; then
-    DEPS_OK=1
-  else
-    echo BUILD ERROR: Cannot find lazbuild binary
-    echo SERACH: $LAZBUILD_BIN
+function clean() {
+    echo "BUILD: Clearing the binaries ( $EXAMPLE_BIN; $EXAMPLE_NEW_BIN )"
     echo
-    exit 1
-  fi
-  if [ -f $UBER_PDF_LIB ]; then
-    DEPS_OK=1
-  else
-    echo BUILD ERROR: Cannot find UberPDF .so library
-    echo SERACH: $UBER_PDF_LIB
-    echo
-    exit 1
-  fi
+    rm -f ./$EXAMPLE_BIN ./$EXAMPLE_NEW_BIN
 }
 
 function build() {
-  echo BUILD: Compiling with Build Option lintel64
-  echo
-  $LAZBUILD_BIN --bm=$EXAMPLE_BUILD $EXAMPLE_FILE
+    echo BUILD: Compiling with Build Option $EXAMPLE_BUILD
+    echo
+    $LAZBUILD_BIN --bm=$EXAMPLE_BUILD $EXAMPLE_FILE
 
-  if [ $? = 0 ]; then
-    echo
-    rename
-  else
-    echo
-    echo BUILD ERROR: Compiling failed, will not move binary
-  fi
+    if [ $? = 0 ]; then
+        echo
+        rename
+    else
+        echo
+        echo BUILD ERROR: Compiling failed, will not move binary
+    fi
 }
 
 function rename() {
-  echo BUILD: Renaming $EXAMPLE_BIN to $EXAMPLE_NEW_BIN
-  echo
-  mv ./$EXAMPLE_BIN ./$EXAMPLE_NEW_BIN
+    echo BUILD: Renaming $EXAMPLE_BIN to $EXAMPLE_NEW_BIN
+    echo
+    mv ./$EXAMPLE_BIN ./$EXAMPLE_NEW_BIN
 }
 
 ## MAIN
 ##
+echo UberPDF Example build script for $UBER_PLATFORM
+echo
 check_dependencies
 
 if [ "x$DEPS_OK" = "x1" ]; then
-  clean
-  build
+    clean
+    build
 fi
 ##
