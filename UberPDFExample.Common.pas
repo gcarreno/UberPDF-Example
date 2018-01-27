7f5f80ab6365768135b0c3ffec10ba86c554e2ab
Gustavo Carreno <guscarreno@gmail.com>
2018-01-27 01:32:22 +0000

    Corrected defines on build options. Added Paths.txt to .gitignore.

--- UberPDFExample.Common.pas
+++ UberPDFExample.Common.pas
@@ -95,34 +95,34 @@ var
   FileName : AnsiString;
 {$ENDIF}
 begin
-  // Load the DLL (if we are dynamic linking) else return true
-  {$IFDEF UBER_PDFSDK_USE_DYNAMIC_LIB_LOADER}
-    frmMain.Log('Loading Dynamic Library');
-    FileName := UBER_PDFSDK_SO_NAME;
-    Result := (UberPdfSdkDynLibLoad(pAnsiChar(FileName)) <> UBER_FALSE);
-    exit;
-  {$ENDIF}
+// Load the DLL (if we are dynamic linking) else return true
+{$IFDEF UBER_PDFSDK_USE_DYNAMIC_LIB_LOADER}
+  frmMain.Log('Loading Dynamic Library');
+  FileName := UBER_PDFSDK_SO_NAME;
+  Result := (UberPdfSdkDynLibLoad(pAnsiChar(FileName)) <> UBER_FALSE);
+  exit;
+{$ENDIF}
   Result := True;
 end;
 
 function UnLoadUberPDFSdk: Boolean;
 begin
-  // unload the DLL (if we are dynamic linking) else return true
-  {$IFDEF UBER_PDFSDK_USE_DYNAMIC_LIB_LOADER}
-    frmMain.Log('Unloading Dynamic Library');
-    Result := (UberPdfSdkDynLibUnload() <> UBER_FALSE);
-    exit;
-  {$ENDIF}
+// unload the DLL (if we are dynamic linking) else return true
+{$IFDEF UBER_PDFSDK_USE_DYNAMIC_LIB_LOADER}
+  frmMain.Log('Unloading Dynamic Library');
+  Result := (UberPdfSdkDynLibUnload() <> UBER_FALSE);
+  exit;
+{$ENDIF}
   result := True;
 end;
 
 function IsUberPDFSdkLoaded: Boolean;
 begin
-  // test if the DLL is loaded (if we are dynamic linking) else return true
-  {$IFDEF UBER_PDFSDK_USE_DYNAMIC_LIB_LOADER}
-    Result := (IsUberPdfSdkDynLibLoaded() <> UBER_FALSE);
-    exit;
-  {$ENDIF}
+// test if the DLL is loaded (if we are dynamic linking) else return true
+{$IFDEF UBER_PDFSDK_USE_DYNAMIC_LIB_LOADER}
+  Result := (IsUberPdfSdkDynLibLoaded() <> UBER_FALSE);
+  exit;
+{$ENDIF}
   Result := True;
 end;
 
